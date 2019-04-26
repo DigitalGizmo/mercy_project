d6306a2eecd01eb25c9e776d798260239c4b3d1d
Don Button <donpublic@digitalgizmo.com>
2019-04-26 10:31:50 -0400

    third picture

--- js/mercy.js
+++ js/mercy.js
@@ -2,8 +2,10 @@
 
 $(document).ready(function(){
 
-	const $photo1 = $('#photo1'),
-		$photo2 = $('#photo2');
+	const photo1 = $('#photo1'),
+		photo2 = $('#photo2'),
+		photo3 = $('#photo3')
+		;
 
 	var screenWidth = $(window).width();
 
@@ -12,52 +14,49 @@ $(document).ready(function(){
 	  // instantiate timeline
 	  const tl = new TimelineMax();
 	  // set initial
-
-	  tl.set($photo1, {autoAlpha: .1, xPercent: -100})
-	  	// .set($photo2, {autoAlpha: .1, y:300, xPercent: -100});  
-	  	// .set($photo2, {autoAlpha: .1, y:300, x:0 - $photo2.width()});  
-	  	.set($photo2, {autoAlpha: .1, 
-	  		y:300, 
-	  		// transform:translateX(- $photo2.width())
-	  		// -webkit-transform:translateX(-$photo2.width())
-	  		// -webkit-transform:translateX("100px")
-	  		x:0 - $photo2.width() - $photo1.width()
-	  	});  
-	  // tl.set($photo1, {autoAlpha: .1, x:-200})
-	  // 	.set($photo2, {autoAlpha: .1, y:300, x:-900});  
-
+	  tl.set(photo1, {autoAlpha: .1, xPercent: -100})
+	  	.set(photo2, {autoAlpha: .1, y:300, xPercent: -100})
+	  	.set(photo3, {autoAlpha: .1, y:100, xPercent: -100}) // , xPercent: -100
+	  	// .set(photo3, {autoAlpha: .1, y:100, x:screenWidth + photo3.width()}) // , xPercent: -100
+	  	;  
 	  // return timeline
 	  return tl;
 	}
 
-	/*  could also use a single TweenMax tween with 
-	repeat:1, yoyo:true, repeatDelay:1 OR a 
-	TweenLite using a SlowMo ease with yoyoMode:true.
 
-	xPercent: -100
-
-	*/
 	function movePhoto1() {
 		var tl = new TimelineMax(0);
-		tl.to($photo1, 2, {autoAlpha:1})
+		tl.to(photo1, 5, {autoAlpha:1, repeat:1, yoyo:true})
+		  // to(photo1, 2, {autoAlpha:1})
 		  // start the positional tween at time of 0
 		  // x is left edge of photo
-		  .to($photo1, 10, {x:screenWidth + $photo1.width(), ease:Linear.easeNone}, 0)
+		  .to(photo1, 10, {x:screenWidth + photo1.width(), ease:Linear.easeNone}, 0)
 		  // begin fade out 1 second before positional tween ends
-		  .to($photo1, 2, {autoAlpha:.2}, "-=2");
+		  // .to(photo1, 2, {autoAlpha:.2}, "-=2")
+		  ;
 		return tl;
 	}
 
 	function movePhoto2() {
 		var tl = new TimelineMax({delay:2}, 0);
 		
-		// tl.to($photo2, 7, {x: "1000px", ease:Linear.easeNone, delay:2} );
+		// console.log(" -- screenWidth: " + screenWidth);
+		// console.log(" -- photo2 width: " + photo2.width());
 
-		tl.to($photo2, 2, {autoAlpha:1})
-		  //start the positional tween at time of 0
-		  .to($photo2, 9, {x:screenWidth + $photo2.width(), ease:Linear.easeNone}, 0)
-		  // //begin fade out 1 second before positional tween ends
-		  .to($photo2, 3, {autoAlpha:0}, "-=3");
+		tl.to(photo2, 4.5, {autoAlpha:1, repeat:1, yoyo:true})
+		  .to(photo2, 9, {x:screenWidth + photo2.width(), ease:Linear.easeNone}, 0);
+
+		return tl;
+	}
+
+	function movePhoto3() {
+		var tl = new TimelineMax({delay:4}, 0);
+		console.log(" -- screenWidth: " + screenWidth);
+		console.log(" -- photo2 width: " + photo2.width());
+		tl.to(photo3, 4.5, {autoAlpha:1, repeat:1, yoyo:true})
+		  .from(photo3, 9, {x:screenWidth + photo3.width(), ease:Linear.easeNone}, 0);
+		  // .to(photo3, 9, {xPercent: -100, ease:Linear.easeNone}, 0);
+		  // .from(photo3, 9, {x:screenWidth, ease:Linear.easeNone}, 0);
 
 		return tl;
 	}
@@ -70,6 +69,7 @@ $(document).ready(function(){
 
 	master.add(setup())
 		.add(movePhoto1(), 0)
-		.add(movePhoto2(), 0);
+		.add(movePhoto2(), 0)
+		.add(movePhoto3(), 0);
 
 }); // end doc ready
