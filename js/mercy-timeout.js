e4b5c959438934c2d81e65daae996cb64d32142f
Don Button <donpublic@digitalgizmo.com>
2019-05-06 15:15:33 -0400

    video 3 and longer timout

--- js/mercy-timeout.js
+++ js/mercy-timeout.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	// const minute =
 	setTimeout(function(){
 	  window.location.href='index.html';
-	}, (2 * 60 * 1000));
+	}, (3 * 60 * 1000));
 	// minues * seconds * milliseconds
 
 }); // end doc ready
