051acbb4f53d5cad2a02e53ee097896fb9c26a7e
Don Button <donpublic@digitalgizmo.com>
2019-05-09 15:27:07 -0400

    add crawl to Touch title

--- js/mercy.js
+++ js/mercy.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 	/* --- Attract loop animation --- */
 	const photos = [$('#photo1'), $('#photo2'), $('#photo3'), 
 		$('#photo4'), $('#photo5'), $('#photo6')];
+	const touchTitle = $('#touch-title')
 	var screenWidth = $(window).width();
 	const speedFactor = 3;
 
@@ -26,6 +27,7 @@ $(document).ready(function(){
 	  	.set(photos[3], {autoAlpha: 0, yPercent:55, xPercent: -100})
 	  	.set(photos[4], {autoAlpha: 0, yPercent:15, xPercent: -100})
 	  	.set(photos[5], {autoAlpha: 0, yPercent:45, xPercent: -100})
+	  	.set(touchTitle, {autoAlpha: .0})
 	  	// .set(photo3, {autoAlpha: .1, y:100, x:screenWidth + photo3.width()}) 
 	  	;  
 	  // return timeline
@@ -46,6 +48,18 @@ $(document).ready(function(){
 		return tl;
 	}
 
+	function moveTitle(delaySecs, durationSecs) {
+		var tl = new TimelineMax({delay:delaySecs}, 0); // , repeat:1
+
+		var rightEnd = $(window).width() - ($(window).width() * .4);
+		console.log(" -- rightEnd: " + rightEnd);
+
+		tl.to(touchTitle, (durationSecs/2), {autoAlpha:1, repeat:1, yoyo:true})
+		  .to(touchTitle, durationSecs, {x:rightEnd, ease:Linear.easeNone}, 0);
+
+		return tl;
+	}
+
 	// instantiate master timeline
 	var master = new TimelineMax({repeat:-1} );
 	// nest and call functions with timelines
@@ -53,11 +67,12 @@ $(document).ready(function(){
 
 	master.add(setup())
 		// photoIndex, delaySecs, durationSecs, forward
+		.add(moveTitle((0 * speedFactor), (12 * speedFactor)), 0)
 		.add(movePhoto(0, (0 * speedFactor), (4 * speedFactor), true), 0)
 		.add(movePhoto(1, (2 * speedFactor), (3 * speedFactor), true), 0)
 		.add(movePhoto(2, (4 * speedFactor), (4 * speedFactor), false), 0)
 		.add(movePhoto(3, (5 * speedFactor), (4 * speedFactor), true), 0)
-		.add(movePhoto(4, (7 * speedFactor), (4.5 * speedFactor), false), 0)
+		.add(movePhoto(4, (7 * speedFactor), (5 * speedFactor), false), 0)
 		.add(movePhoto(5, (8 * speedFactor), (4 * speedFactor), true), 0)
 		;
 
